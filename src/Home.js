7971f85fc5f2948fae9dd75fc505d5d437ff2f46
ericzlmd <ericzlmd@gmail.com>
2021-01-11 19:54:21 -0500

    Added fetch error handling

--- src/Home.js
+++ src/Home.js
@@ -1,46 +1,52 @@
 import { useEffect, useState } from 'react';
 import BlogList from './BlogList';
 
+{
+  /*
+   * We're gonna be using json-server, instead of creating a db from scratch.
+   * Run command 👇
+   *
+   * $ npx json-server --watch data/db.json --port 8000
+   *
+   * ENDPOINTS:
+   * /blogs         GET       Fetch all blogs
+   * /blogs/{id}    GET       Fetch a single blog
+   * /blogs         POST      Add a new blog
+   * /blogs/{id}    DELETE    Delete a blog
+   */
+}
+
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
-
-  // const handleDelete = id => {
-  //   const newBlogs = blogs.filter(blog => blog.id !== id);
-  //   setBlogs(newBlogs);
-  // };
-
-  {
-    /*
-     * We're gonna be using json-server, instead of creating a db from scratch.
-     * Run command 👇
-     *
-     * $ npx json-server --watch data/db.json --port 8000
-     *
-     * ENDPOINTS:
-     * /blogs         GET       Fetch all blogs
-     * /blogs/{id}    GET       Fetch a single blog
-     * /blogs         POST      Add a new blog
-     * /blogs/{id}    DELETE    Delete a blog
-     */
-  }
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
         .then(res => {
+          // console.log(res);
+          if (!res.ok) {
+            throw Error('Could not fetch data from resource!');
+          }
           return res.json();
         })
         .then(data => {
           console.log(data);
           setBlogs(data);
           setIsPending(false);
+          setError(null);
+        })
+        .catch(err => {
+          setIsPending(false);
+          setError(err.message);
         });
     }, 1000);
   }, []); // useEffect dependancy
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
