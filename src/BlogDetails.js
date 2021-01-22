b9eb73a683255e64489be7566daade9dac90047d
ericzlmd <ericzlmd@gmail.com>
2021-01-21 21:17:59 -0500

    Maded each blog entry a <Link> with dynamic routing with id as parameters

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 function BlogDetails() {
+  const { id } = useParams(); // grab route parameters
+
   return (
     <div className="blog-details">
-      <h2>Blog Details</h2>
+      <h2>Blog Details - {id}</h2>
     </div>
   );
 }
