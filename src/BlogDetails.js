faf1c6e401a8cb5d464bb65b3d5f9d71abffc5d6
ericzlmd <ericzlmd@gmail.com>
2021-01-25 21:02:36 -0500

    Implemented useFetch() in BlogDetails.js to load each blog entry

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import useFetch from './useFetch';
 
 function BlogDetails() {
   const { id } = useParams(); // grab route parameters
+  const { data: blog, error, isPending } = useFetch(
+    'http://localhost:8000/blogs/' + id
+  );
 
   return (
     <div className="blog-details">
-      <h2>Blog Details - {id}</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error} </div>}
+      {blog && (
+        <article>
+          <h2> {blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 }
