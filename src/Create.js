f1a28abcfd9c2974a01d92a6d4d85be35380484a
ericzlmd <ericzlmd@gmail.com>
2021-02-08 21:29:16 -0500

    Added <form> elements to create new blogs

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Create() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
+      <form action="">
+        <label>Blog Title</label>
+        <input
+          type="text"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          required
+        />
+        <label>Blog Body:</label>
+        <textarea
+          value={body}
+          onChange={e => setBody(e.target.value)}
+          required
+        ></textarea>
+        <label>Blog Author</label>
+        <select value={author} onChange={e => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
   );
 }
